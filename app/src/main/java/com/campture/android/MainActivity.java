5620e9ff733d544cd44254624b59b618a2e84459
Arnav Gupta <championswimmer@gmail.com>
2015-10-31 00:45:31 +0530

    update sidebar

--- app/src/main/java/com/campture/android/MainActivity.java
+++ app/src/main/java/com/campture/android/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
+        if (id == R.id.nav_discover) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_recommended) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_wishlist) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_blog) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_contact_us) {
 
         }
 
